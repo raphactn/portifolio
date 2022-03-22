5fe5ce8156764a2007f8bb38c5d214d13b25270b
raphactn <raphael.d.caetano48@gmail.com>
2022-03-22 14:21:50 -0300

    alteração resolução

--- components/works.js
+++ components/works.js
@@ -9,6 +9,15 @@ function works(){
         speed: 500,
         responsive: [
           {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
